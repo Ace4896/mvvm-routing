15ba6a82ecad603cc69e02a392b710461f36b08a
Jon Pacheco <jon.pacheco@tutanota.com>
2022-08-06 12:27:32 +0100

    feat: add unit tests for Activator and Router

---
+++ tests/MvvmRouting.Tests/ActivatorTests.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MvvmRouting.Tests;
+
+public class ActivatorTests
+{
+    private class ActivatableViewModel : IActivatableViewModel
+    {
+        public Activator Activator { get; }
+
+        public int ActivationCount = 0;
+        public int DeactivationCount = 0;
+
+        public ActivatableViewModel()
+        {
+            Activator = new(this);
+        }
+
+        public void OnActivation()
+        {
+            ActivationCount++;
+        }
+
+        public void OnDeactivation()
+        {
+            DeactivationCount++;
+        }
+    }
+
+    [Fact]
+    public void Activation_Deactivation_Works()
+    {
+        var viewModel = new ActivatableViewModel();
+
+        // Should be deactivated by default
+        Assert.False(viewModel.Activator.Activated);
+        Assert.Equal(0, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+
+        // Activate the ViewModel
+        viewModel.Activator.Activate();
+
+        Assert.True(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+
+        // Deactivate the ViewModel
+        viewModel.Activator.Deactivate();
+
+        Assert.False(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(1, viewModel.DeactivationCount);
+    }
+
+    [Fact]
+    public void Activate_OnlyCallsWhenDeactivated()
+    {
+        var viewModel = new ActivatableViewModel();
+
+        // Should be deactivated by default
+        Assert.False(viewModel.Activator.Activated);
+        Assert.Equal(0, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+
+        // Activate the ViewModel
+        viewModel.Activator.Activate();
+
+        Assert.True(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+
+        // Attempt to re-activate the ViewModel; shouldn't call OnActivation anymore
+        viewModel.Activator.Activate();
+
+        Assert.True(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+    }
+
+    [Fact]
+    public void Deactivate_OnlyCallsWhenActivated()
+    {
+        var viewModel = new ActivatableViewModel();
+
+        // Should be deactivated by default
+        Assert.False(viewModel.Activator.Activated);
+        Assert.Equal(0, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+
+        // Activate the ViewModel
+        viewModel.Activator.Activate();
+
+        Assert.True(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(0, viewModel.DeactivationCount);
+
+        // Deactivate the ViewModel
+        viewModel.Activator.Deactivate();
+
+        Assert.False(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(1, viewModel.DeactivationCount);
+
+        // Attempt to re-deactivate the ViewModel; shouldn't call OnDeactivation anymore
+        viewModel.Activator.Deactivate();
+
+        Assert.False(viewModel.Activator.Activated);
+        Assert.Equal(1, viewModel.ActivationCount);
+        Assert.Equal(1, viewModel.DeactivationCount);
+    }
+}
---
+++ tests/MvvmRouting.Tests/RouterTests.cs
@@ -0,0 +1,53 @@
+﻿using Xunit;
+
+namespace MvvmRouting.Tests;
+
+public class RouterTests
+{
+    private class HostViewModel : IHostViewModel
+    {
+        public Router Router { get; } = new();
+    }
+
+    private class PageViewModel : IPageViewModel
+    {
+        public IHostViewModel? HostViewModel { get; }
+
+        public PageViewModel(IHostViewModel? hostViewModel)
+        {
+            HostViewModel = hostViewModel;
+        }
+    }
+
+    [Fact]
+    public void Navigate_ChangesViewModel_RaisesPropertyChanged()
+    {
+        HostViewModel hostViewModel = new();
+        IPageViewModel? propertyChangedViewModel = null;
+
+        hostViewModel.Router.PropertyChanged += (_, args) =>
+        {
+            if (args.PropertyName == nameof(Router.CurrentViewModel))
+            {
+                propertyChangedViewModel = hostViewModel.Router.CurrentViewModel;
+            }
+        };
+
+        // Initial ViewModel should be null
+        Assert.Null(hostViewModel.Router.CurrentViewModel);
+
+        // Navigate to a new page
+        PageViewModel firstPage = new(hostViewModel);
+        hostViewModel.Router.Navigate(firstPage);
+
+        Assert.Equal(firstPage, hostViewModel.Router.CurrentViewModel);
+        Assert.Equal(firstPage, propertyChangedViewModel);
+
+        // Then to another page
+        PageViewModel secondPage = new(hostViewModel);
+        hostViewModel.Router.Navigate(secondPage);
+
+        Assert.Equal(secondPage, hostViewModel.Router.CurrentViewModel);
+        Assert.Equal(secondPage, propertyChangedViewModel);
+    }
+}
